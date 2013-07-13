7b85a25df70a5351c6d168f4501169957bcbdeb1
Ventile <gijsvanschayk@gmail.com>
2013-07-13 05:42:19 +0000

    retry

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var fs = require('fs');
+var buf = require('buf');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  response.send(buf.toString(fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
